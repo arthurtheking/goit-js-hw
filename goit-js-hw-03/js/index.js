f35c922032d205ade6bd7757b8fb84849326bfa0
Arthur <bardartur@gmail.com>
2020-02-04 21:46:39 +0200

    added prettier cnfg

--- goit-js-hw-03/js/index.js
+++ goit-js-hw-03/js/index.js
@@ -33,25 +33,29 @@ console.log(c);
 const e = [12, 2, 3];
 const y = [1, 4];
 
-const g = [...e, ...y]
+const g = [...e, ...y];
 
-console.log(g)
+console.log(g);
 
 // Spread :
 // 1 при передаче аргументов
 // 2 в правой части присваивания
 
-
 const hotel = {
-    name: 'Resort',
-    stars: 5,
-    place: {
-        country: 'Ukraine',
-        city: 'Lviv'
-    },
-    price: 900,
+  name: "Resort",
+  stars: 5,
+  place: {
+    country: "Ukraine",
+    city: "Lviv"
+  },
+  price: 900
 };
 
-const {name, stars, place: {country, city}, price} = hotel;
+const {
+  name,
+  stars,
+  place: { country, city },
+  price
+} = hotel;
 
-console.log(name, stars, country, price, city)
+console.log(name, stars, country, price, city);
