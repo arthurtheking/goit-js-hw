544bd0ff5f46bca7942b054a76024405e35f675a
Arthur <bardartur@gmail.com>
2020-03-09 14:56:41 +0200

    upl hw 5-6

--- goit-js-hw-06/js/task-10.js
+++ goit-js-hw-06/js/task-10.js
@@ -2,25 +2,23 @@
 
 import users from "./users.js";
 
-
 const getSortedUniqueSkills = users => {
-   let allSkills = [];
-   users.map(user => {
-        allSkills.push(...user.skills)
-    })
+  let allSkills = [];
+  users.map(user => {
+    allSkills.push(...user.skills);
+  });
 
-    const unicSkills = allSkills.reduce((acc, skill) => {
-        return {
-            ...acc,
-            [skill]: acc.hasOwnProperty(skill) ? (acc[skill] += 1) : 1
-        }
-    }, {});
-    
-    const sortedSkills = Object.keys(unicSkills).sort();
+  const unicSkills = allSkills.reduce((acc, skill) => {
+    return {
+      ...acc,
+      [skill]: acc.hasOwnProperty(skill) ? (acc[skill] += 1) : 1
+    };
+  }, {});
 
-    return sortedSkills;
+  const sortedSkills = Object.keys(unicSkills).sort();
 
-  };
-  
-  console.log(getSortedUniqueSkills(users));
-  // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+  return sortedSkills;
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
